352399bc8d98d13e9da0e1df5e8a1f067827d423
daniel de brito <danieldebrito@outlook.com>
2023-05-13 15:49:17 -0300

    splash

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, ModalController } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
+import { SplashComponent } from './splash/splash.component';
 
 @Component({
   selector: 'app-root',
@@ -12,5 +13,16 @@ import { CommonModule } from '@angular/common';
 export class AppComponent {
   public environmentInjector = inject(EnvironmentInjector);
 
-  constructor() {}
+  constructor(private modalController: ModalController) {
+    this.presentSplash();
+  }
+
+
+  async presentSplash(){
+    const modal = await this.modalController.create({
+      component: SplashComponent,
+      cssClass: 'my-custom-class'
+    });
+    return await modal.present();
+  }
 }
---
+++ src/app/splash/splash.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { IonicModule } from '@ionic/angular';
+
+
+@Component({
+  selector: 'app-splash',
+  templateUrl: './splash.component.html',
+  styleUrls: ['./splash.component.scss'],
+  standalone: true,
+  imports: [IonicModule]
+
+})
+export class SplashComponent  implements OnInit {
+
+  private audioContext: AudioContext = new AudioContext();
+  private sound: HTMLAudioElement = new Audio('src');
+
+  constructor( private modalController: ModalController) { }
+
+
+  ionViewDidEnter(){
+    setTimeout(() => {
+      this.modalController.dismiss();
+    }, 3200);
+  }
+
+  public reproducirSonido(): void {
+    const source = this.audioContext.createMediaElementSource(this.sound);
+    source.connect(this.audioContext.destination);
+    this.sound.play();
+  }
+
+  ngOnInit() {
+    this.reproducirSonido();
+  }
+
+
+
+}
