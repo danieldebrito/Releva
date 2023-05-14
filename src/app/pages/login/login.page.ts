eef0ce411b9c7ae5242fb2dd8fc121f328984207
daniel de brito <danieldebrito@outlook.com>
2023-05-14 18:10:03 -0300

    login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,23 +1,91 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
+import { UsersService } from 'src/app/services/users.service';
+import { Usuario } from 'src/app/class/usuario';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule, RouterModule]
+  imports: [IonicModule, CommonModule, FormsModule, ReactiveFormsModule, RouterModule]
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  public usuarios: Usuario[] = [];
 
-  ngOnInit() {
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+  });
+
+  public constructor(private usuariosSv: UsersService) { }
+
+  async onLogin(): Promise<void> {
+    const { email, password } = this.loginForm.value;
+    if (email && password) {
+      try {
+        // const user = await this.authService.SignIn(email, password);
+      } catch (error) {
+        console.log(error);
+      }
+    }
   }
 
-  login(a: any){return a;}
+  public AutoSignIn(iniciales: string) {
+    switch (iniciales) {
+      case 'ADM':
+        this.loginForm.setValue({
+          email: 'admin@admin.com',
+          password: '1111'
+        });
+        break;
+      case 'INV':
+        this.loginForm.setValue({
+          email: 'invitado@invitado.com',
+          password: '2222'
+        });
+        break;
+      case 'USR':
+        this.loginForm.setValue({ 
+          email: 'usuario@usuario.com', 
+          password: '3333' 
+        });
+        break;
+      case 'ANN':
+        this.loginForm.setValue({ 
+          email: 'anonimo@anonimo.com', 
+          password: '4444'
+         });
+        break;
+      case 'TST':
+        this.loginForm.setValue({ 
+          email: 'tester@tester.com', 
+          password: '5555'
+         });
+        break;
+      default:
+        this.loginForm.setValue({ 
+          email: 'ERROR', 
+          password: 'ERROR'
+         });
+        break;
+    }
+  }
+
+  public errorFalse(){}
+
+
+  ngOnInit() {
+    this.usuariosSv.getItems().subscribe(res => {
+      this.usuarios = res;
+      console.log(res);
+    });
+  }
+
+  login(a: any) { return a; }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,11 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
-import { environment } from './environments/environment';
-
 import { defineCustomElements } from '@ionic/pwa-elements/loader';
+
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app'
+import { getFirestore, provideFirestore } from '@angular/fire/firestore'
+import { environment } from './environments/environment'; 
 
 // Call the element loader after the platform has been bootstrapped
 defineCustomElements(window);
@@ -19,7 +21,11 @@ if (environment.production) {
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    importProvidersFrom(IonicModule.forRoot({})),
+    importProvidersFrom(
+      IonicModule.forRoot({}),
+      provideFirebaseApp( () => initializeApp(environment.firebase)),
+      provideFirestore( () => getFirestore() )
+      ),
     provideRouter(routes),
   ],
 });
